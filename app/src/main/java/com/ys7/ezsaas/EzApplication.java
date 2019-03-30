604b8ea5092e8270315a1abfa1bf87f1a50bc262
ezvizsaas <Hik123456>
2019-03-30 10:56:14 +0800

    删除多余代码

--- app/src/main/java/com/ys7/ezsaas/EzApplication.java
+++ app/src/main/java/com/ys7/ezsaas/EzApplication.java
@@ -15,9 +15,5 @@ public class EzApplication extends MultiDexApplication {
         super.onCreate();
         EzSaaSSDK.openDebug(true);
         EzSaaSSDK.init(this,"431cf2e3495b11e9acb91aca0191c622");
-        //EzSaaSSDK.getInstance().setToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NTMwNDkwMDUsInNhYXNVc2VySWQiOjkwODU2LCJ1c2VyX25hbWUiOiI5MDg1NiIsImp0aSI6IjhjZGFmZGIyLTUxNzctNDg5NC1hZmVmLWVjYzVhYzg2ZmE4MiIsImNsaWVudF9pZCI6InlzN0FwcCIsInNjb3BlIjpbImFsbCJdfQ.QmpbK2oeYxjRAhtREj5s7CTzv31s7Ari9RC6e-fGebg");
-        //EzSaaSSDK.getInstance().setToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiI5MDg1NiIsInNjb3BlIjpbImFsbCJdLCJ1c2VyVHlwZSI6ImVudGVycHJpc2UiLCJleHAiOjE1NTMzMDcyMzgsInNhYXNVc2VySWQiOjkwODU2LCJqdGkiOiIwNTYzYjYzMC00ZGEzLTRlZDAtYTc2NC1jYjA4MzJlMjUxNGEiLCJjbGllbnRfaWQiOiJ5czdBcHAifQ.kXgVzGaYSlCvxBm4eCOsUwVXgFbZLtTHi5U6wibSDEM");
-        LG.e("EzSaaSSDK.getInstance().setToken======================");
-        //PermissionManager.getInstance().setPermissionStrategy(null);
     }
 }
